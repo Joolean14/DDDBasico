dadbcc3e0788389b8fc3ec8b43d35a370a614f75
julian <Joolean14>
2021-04-22 19:15:26 -0500

    [Setup]

---
+++ src/main/java/com/SistemaDeGestion/DDDBasico/DddBasicoApplication.java
@@ -0,0 +1,13 @@
+package com.SistemaDeGestion.DDDBasico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddBasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddBasicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SistemaDeGestion/DDDBasico/DddBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SistemaDeGestion.DDDBasico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
